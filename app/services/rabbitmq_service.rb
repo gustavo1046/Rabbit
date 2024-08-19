2cd85d52d873941b8c79af132efa871d19cfa42d
Gustavo Oliveira <gustavo1046@hotmail.com>
2024-08-19 18:55:18 -0300

    created function that sends messages to queue

--- app/services/rabbitmq_service.rb
+++ app/services/rabbitmq_service.rb
@@ -1,10 +1,10 @@
 class RabbitMQService
-    def self.queue(queue_name)
-      @queues ||= {}
-      @queues[queue_name] ||= begin
-        channel = $conn.create_channel
-        channel.queue(queue_name)
-      end
+  def self.queue(queue_name)
+    @queues ||= {}
+    @queues[queue_name] ||= begin
+      channel = $conn.create_channel
+      queue = channel.queue(queue_name, durable: true)
+      queue
     end
   end
-  
+end
